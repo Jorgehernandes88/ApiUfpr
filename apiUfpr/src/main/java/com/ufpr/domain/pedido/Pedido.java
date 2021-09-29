bfb701ae99bc02341829930d1272b750941b4cb0
Jorge Hernandesm <jorgehernandes88@gmail.com>
2021-09-28 22:57:54 -0300

    Post @OneToMany Pedidos e Itens

--- apiUfpr/src/main/java/com/ufpr/domain/pedido/Pedido.java
+++ apiUfpr/src/main/java/com/ufpr/domain/pedido/Pedido.java
@@ -39,7 +39,7 @@ public class Pedido {
 	
 	@OneToMany(cascade = CascadeType.ALL)
 	@JoinColumn(name="Pedido_idPedido")
-	private List<ItemDoPedido> itensDoPedido;
+	private List<ItemDoPedido> itensDoPedido = new ArrayList<>();
 	
 	public Pedido() {
 		
