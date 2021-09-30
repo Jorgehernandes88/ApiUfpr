79e227ec147d0e3ac20a4023cebadaacb1cf5855
pedrvaldir <pedrvaldir@gmail.com>
2021-09-30 18:00:32 -0300

    listar cliente em pedido

--- apiUfpr/src/main/java/com/ufpr/api/PedidoController.java
+++ apiUfpr/src/main/java/com/ufpr/api/PedidoController.java
@@ -1,17 +1,15 @@
 package com.ufpr.api;
 
-import java.util.List;
 import java.util.Optional;
 
-import org.apache.catalina.connector.Response;
+import com.ufpr.domain.Cliente;
+import com.ufpr.domain.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +26,8 @@ public class PedidoController {
 	
 	@Autowired
 	private PedidoService service;
+	@Autowired
+	private ClienteService clienteService;
 	private ItemDoPedidoService servicePedido;
 	
 	@GetMapping()
@@ -36,13 +36,13 @@ public class PedidoController {
 	}
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<Pedido> get( @PathVariable("id") Long id) {
+	public ResponseEntity<Pedido> get(@PathVariable("id") Long id) {
 		
 		Optional<Pedido> pedido = service.getPedidoById(id);
 		
 		if(pedido.isPresent())
 		{
-			return ResponseEntity.ok(pedido.get());
+			return ResponseEntity.ok().body(pedido.get());
 		}else {
 			return ResponseEntity.notFound().build();
 		}
