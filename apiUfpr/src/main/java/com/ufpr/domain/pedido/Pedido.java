f887e0e9ce3ee309fc653f3be3c7577f9826d5ae
pedrvaldir <pedrvaldir@gmail.com>
2021-09-30 18:25:06 -0300

    Ajusta pedidos

--- apiUfpr/src/main/java/com/ufpr/domain/pedido/Pedido.java
+++ apiUfpr/src/main/java/com/ufpr/domain/pedido/Pedido.java
@@ -4,20 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.persistence.Transient;
+import javax.persistence.*;
 import javax.xml.crypto.Data;
 
+import com.ufpr.domain.Cliente;
 import com.ufpr.domain.itemDoPedido.ItemDoPedido;
 
 import net.bytebuddy.implementation.bind.annotation.IgnoreForBinding;
@@ -33,10 +23,12 @@ public class Pedido {
 	private Long idPedido;
 	
 	private String data;
-	
-	@Column(name = "Cliente_idCliente")
-	private String idCliente;
-	
+
+	@ManyToOne
+	@JoinColumn(name="Cliente_idCliente", nullable=false)
+	private Cliente cliente;
+
+
 	@OneToMany(cascade = CascadeType.ALL)
 	@JoinColumn(name="Pedido_idPedido")
 	private List<ItemDoPedido> itensDoPedido = new ArrayList<>();
@@ -65,13 +57,13 @@ public class Pedido {
 	}
 
 
-	public String getIdCliente() {
-		return idCliente;
+	public Cliente getCliente() {
+		return cliente;
 	}
 
 
-	public void setIdCliente(String idCliente) {
-		this.idCliente = idCliente;
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
 	}
 
 	public List<ItemDoPedido> getItensDoPedido() {
